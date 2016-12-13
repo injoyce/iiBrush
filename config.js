5187f58923c75584a7445d140838c3ca3d7ac853
injoyce <zhangpingping891@gmail.com>
2016-12-13 23:19:19 +0900

    change themes

---
+++ config.js
@@ -0,0 +1,11 @@
+// Theme Config, check the documentation before edit this file
+
+var theme_config = {
+	twitter_app_id : '807957751364087808', // Widget id created on https://twitter.com/settings/widgets, correct example: 658074580351873024
+	twitter_username : 'Joyce', //Without @ just for the title, correct example: envato
+	twitter_limit: 5, // Number of tweets to show
+	flickr_id: '48600098927@N01', // You can get it here: http://idgettr.com/
+	flickr_photos_limit: 6, // How many photos you want to show
+	disqus_shortname: 'iibrush666', // You can create your own Disqus Comments here: https://disqus.com/admin/create/
+};
+
